6ea3268b6c14451d9dbe042623e1aa2d1a4a8632
alirn76 <alirn76@yahoo.com>
2019-10-27 16:07:44 +0330

    First Commit

---
+++ mybot.py
@@ -0,0 +1,25 @@
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+
+def main():
+    updater = Updater('989051924:AAE6y8FsR3A4E_ptuZFSg1z_jSG5oq8bFuA')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop', bop))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
